0a855e8477dc251b2d6f753ba220e678d2a32c55
elsie-huang <yunqian0522@gmail.com>
2025-02-26 20:31:06 +0800

    Add files via upload

--- hello-world.c
+++ hello-world.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-    printf("Hello, World");
+    int x, y;
+    char A[16], B[16], C[16];
+    int a,b,c;
+
+    printf("\n Hello, World!\n\n");
+
+    for(x=1; x<10; x++)
+    {
+        for(y=1; y<10; y++)
+        {
+            printf("%2dX%2d=%2d ", x, y, x*y);
+        }
+        printf("\n");
+    }
+
+    printf("\n Solving linear equations with one unknown\n");
+    printf("Solving ax+b=c\n");
+
+retry:
+    printf("input a (!=0 integer) = ");
+    scanf("%s", A);
+    a=atoi(A);
+    if(a==0) goto retry;
+    printf("input b (integer) = ");
+    scanf("%s", B);
+    printf("input c (integer) = ");
+    scanf("%s", C);
+    printf("Solving %sx+%s=%s", A,B,C); 
+    b=atoi(B);
+    c=atoi(C);
+    printf("\t.....\t x=%f\n", ((float)(c-b)/(float)a));
+
     return 0;
 }
